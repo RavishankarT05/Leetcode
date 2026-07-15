989c105e613cee542388809e06781042c28454b4
RavishankarT05 <ravishankart05122007@gmail.com>
2026-07-15 18:04:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.86%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        a=[]
+        k=celsius+273.1500
+        a.append(k)
+        f=celsius*1.800+32
+        a.append(f)
+        return a
+
+        
