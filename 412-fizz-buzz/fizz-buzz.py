3b5979ce3eb43b14463c9c8a13e301ec7b0a94ed
RavishankarT05 <ravishankart05122007@gmail.com>
2026-07-15 20:35:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.06%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        a=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                a.append("FizzBuzz")
+            elif i%3==0:
+                a.append("Fizz")
+            elif i%5==0:
+                a.append("Buzz")
+            else:
+                a.append(str(i))
+        # a.remove(a[0])
+        return a
+
+        
