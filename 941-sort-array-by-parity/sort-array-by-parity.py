8df4e653bb0302f2f6001ed8bae45e5a36a2c262
RavishankarT05 <ravishankart05122007@gmail.com>
2026-07-20 16:45:43 +0530

    Time: 5 ms (23.17%) | Memory: 12.9 MB (68.67%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        l,r=0,0
+        while(r<len(nums)):
+            if(nums[r]%2==0):
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+            r+=1
+        return nums
