fcf4a682494deabd13da4ba4ce3746360aa2c1b1
RavishankarT05 <ravishankart05122007@gmail.com>
2026-07-28 18:00:55 +0530

    Time: 2008 ms (5.22%) | Memory: 13.3 MB (41.30%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,18 +1,17 @@
-# class Solution(object):
-#     def repeatedNTimes(self, nums):
-#         count=0
-#         a=0
-#         b=set(nums)
-#         for i in b:
-#             c=nums.count(i)
-#             if count<c:
-#                 count=c
-#                 a=i
-#         return a
-class Solution:
+class Solution(object):
     def repeatedNTimes(self, nums):
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return num
-            seen.add(num)
+        count=1
+        a=0
+        b=set(nums)
+        for i in b:
+            c=nums.count(i)
+            if count<c:
+                return i
+#         return a
+# class Solution:
+#     def repeatedNTimes(self, nums):
+#         seen = set()
+#         for num in nums:
+#             if num in seen:
+#                 return num
+#             seen.add(num)
