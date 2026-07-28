bbd6cd50cf2646bd4c90fd41e88597696dcffe02
RavishankarT05 <ravishankart05122007@gmail.com>
2026-07-28 18:25:40 +0530

    Time: 64 ms (62.37%) | Memory: 12.6 MB (77.56%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def addToArrayForm(self, num, k):
-        n=int("".join(map(str,num)))
-        # n=n+k
-        return list(map(int,str(n+k)))
+        # n=int("".join(map(str,num)))
+        # # n=n+k
+        # return list(map(int,str(n+k)))
+        num_val = int("".join([str(x) for x in num])) 
+        num_val += k
+        return [int(x) for x in (list(str(num_val)))]
