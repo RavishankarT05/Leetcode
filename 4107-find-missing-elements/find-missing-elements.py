c86e790a783753ee827a295a3357575fcb32f4ab
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-04 15:29:55 +0530

    Time: 7 ms (52.53%) | Memory: 12.4 MB (17.28%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -2,8 +2,6 @@ class Solution(object):
     def findMissingElements(self, nums):
         a=[]
         nums.sort()
-        b=nums[0]
-        c=nums[-1]
         for i in range(nums[0],nums[-1]+1):
             if i not in nums:
                 a.append(i)
