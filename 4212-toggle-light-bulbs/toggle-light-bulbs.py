317867c3ff29f7ba7fb8379e18b6429467c53517
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-06 18:17:54 +0530

    Time: 12 ms (28.13%) | Memory: 12.2 MB (92.19%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.py
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def toggleLightBulbs(self, bulbs):
         a=[]
-        b=set(bulbs)
+        b=sorted(set(bulbs))
         for i in b:
             count=bulbs.count(i)
             if count%2!=0:
                 a.append(i)
-        return sorted(a)
+        return a
             
