bb7b51fdeec388a6b94135e49b6e033d2cc0abe4
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-09 10:14:50 +0530

    Time: 41 ms (26.83%) | Memory: 13.9 MB (55.71%) - LeetSync

--- 561-array-partition/array-partition.py
+++ 561-array-partition/array-partition.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def arrayPairSum(self, nums):
-        # nums.sort()
-        # a,b=0,1
-        # count=0
-        # for _ in range(len(nums)/2):
-        #     count+=min(nums[a],nums[b])
-        #     a+=2
-        #     b+=2
-        # return count
         nums.sort()
-        
-        # Step 2: Sum every second element starting from index 0
+        a=0
+        count=0
+        for _ in range(len(nums)/2):
+            count+=nums[a]
+            a+=2
+        return count
+        nums.sort()
         return sum(nums[::2])
