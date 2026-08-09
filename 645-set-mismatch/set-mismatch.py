dc8db9baed4a7258eb899109cffa791821605992
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-09 22:42:27 +0530

    Time: 3 ms (96.00%) | Memory: 14 MB (24.57%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -2,7 +2,6 @@ class Solution:
     def findErrorNums(self, nums):
         m=sum(nums)-sum(set(nums))
         exp =len(nums)*(len(nums)+1)//2
-        print(exp)
         t= exp-sum(set(nums))
         return  [m,t]
         
