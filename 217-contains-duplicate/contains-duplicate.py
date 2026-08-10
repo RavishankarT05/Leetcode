9422475ac2da1212bf9ef0537b1cb0b3d813924c
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-10 16:05:05 +0530

    Time: 18 ms (77.07%) | Memory: 26 MB (19.96%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,13 +1,3 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        # a=[]
-        # for i in nums:
-        #     if i in a:
-        #         return True
-        #     else:
-        #         a.append(i)
-        # return False
-        if len(nums)==len(set(nums)):
-            return False
-        else:
-            return True
+        return len(nums)!=len(set(nums))
