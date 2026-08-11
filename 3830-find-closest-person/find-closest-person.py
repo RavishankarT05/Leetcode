e5691d1018a1af0ba95a46edcf696c625ab6c1a7
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-11 19:59:26 +0530

    Time: 8 ms (1.08%) | Memory: 12.3 MB (57.19%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findClosest(self, x, y, z):
+        a=max(x,z)-min(x,z)
+        b=max(y,z)-min(y,z)
+        print(a,b)
+        if a<b:
+            return 1
+        elif b<a:
+            return 2
+        else:
+            return 0
