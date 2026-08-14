753e947ad5ee5ef9c0cbb796202df4a082d4e9ab
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-14 21:17:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.09%) - LeetSync

--- 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.py
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def firstMatchingIndex(self, s):
-        # a,b=0,len(s)-1
-        # while a<=b:
-        #     if s[a]==s[b]:
-        #         return a
-        #     a+=1
-        #     b-=1
-        # return -1
-
-
-
-
-
-        count=0
-        for i in range(len(s)):
-            if s[i]==s[len(s)-i-1]:
-                return i
+        a,b=0,len(s)-1
+        while a<=b:
+            if s[a]==s[b]:
+                return a
+            a+=1
+            b-=1
         return -1
+
+
+
+
+
+        # count=0
+        # for i in range(len(s)):
+        #     if s[i]==s[len(s)-i-1]:
+        #         return i
+        # return -1
