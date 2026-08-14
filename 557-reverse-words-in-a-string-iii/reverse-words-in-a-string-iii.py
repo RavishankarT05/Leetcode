d9a3f53e13f66ee2fe83f3d7b703dd6df7d5f0d0
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-14 14:20:40 +0530

    Time: 7 ms (29.73%) | Memory: 12.8 MB (63.19%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -3,13 +3,16 @@ class Solution(object):
         s=list(s.split())
         z=[]
         for i in s:
-            i=list(i)
-            a,b=0,len(i)-1
-            while a<b:
-                i[a],i[b]=i[b],i[a]
-                a+=1
-                b-=1
+        #     i=list(i)
+        #     a,b=0,len(i)-1
+        #     while a<b:
+        #         i[a],i[b]=i[b],i[a]
+        #         a+=1
+        #         b-=1
+        #     y=''.join(i)
+        #     z.append(y)
+        # return ' '.join(z)
+            i=i[::-1]
             y=''.join(i)
             z.append(y)
         return ' '.join(z)
-                
