2f765264704ca9535afa3d5faf726ab6efde4165
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-14 13:21:37 +0530

    Time: 51 ms (19.13%) | Memory: 13.6 MB (45.52%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -4,12 +4,14 @@ class Solution(object):
         a,b=0,len(s)-1
         c=['A','E','I','O','U','a','e','i','o','u']
         while a<b:
-            if s[a] not in c:
+            if s[a] in c:
+                if s[b] in c:
+                    if s[a] in c and s[b] in c:
+                        s[a],s[b]=s[b],s[a]
+                        a+=1
+                        b-=1
+                else:
+                    b-=1
+            else:
                 a+=1
-            if s[b] not in c:
-                b-=1
-            if s[a] in c and s[b] in c:
-                s[a],s[b]=s[b],s[a]
-                a+=1
-                b-=1
         return "".join(s)
