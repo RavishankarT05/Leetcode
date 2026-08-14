5265b150e184bffe660b0290eb81ed05ddc5c6c8
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-14 13:30:43 +0530

    Time: 4 ms (18.53%) | Memory: 12.5 MB (18.66%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        a,b=0,0
+        while a<len(s) and b<len(t):
+            if s[a]==t[b]:
+                a+=1
+                b+=1
+            else:
+                b+=1
+        return a==len(s)
+        
