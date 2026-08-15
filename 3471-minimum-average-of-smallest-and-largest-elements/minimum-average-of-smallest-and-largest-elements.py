5f7b4263124d094f1ff659ff787ebc3f6d17412b
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-15 12:28:54 +0530

    Time: 174 ms (5.26%) | Memory: 12.2 MB (89.47%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.py
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.py
@@ -1,8 +1,29 @@
 class Solution(object):
     def minimumAverage(self, nums):
-        a=[]
-        for i in range(len(nums)/2):
-            a.append((min(nums) + max(nums)) / 2.0)
-            nums.remove(min(nums))
-            nums.remove(max(nums))
-        return min(a)
+        nums.sort()
+        z=[]
+        a,b=0,-1
+        while True:
+            print(nums)
+            z.append((nums[a]+nums[b])/2.0)
+            nums.remove(nums[a])
+            nums.remove(nums[b])
+            if len(nums)==0:
+                break
+        return min(z)
+
+
+
+
+
+
+
+
+
+
+        # a=[]
+        # for i in range(len(nums)/2):
+        #     a.append((min(nums) + max(nums)) / 2.0)
+        #     nums.remove(min(nums))
+        #     nums.remove(max(nums))
+        # return min(a)
