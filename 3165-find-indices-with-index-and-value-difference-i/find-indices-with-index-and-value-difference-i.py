0caa6a597a857d467fa1bf5408b56f81130c004e
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-15 12:39:33 +0530

    Time: 7 ms (13.43%) | Memory: 12.3 MB (91.04%) - LeetSync

---
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findIndices(self, nums, indexDifference, valueDifference):
+        i,j=0,0
+        while i<len(nums):
+            if abs(i - j) >= indexDifference and abs(nums[i] - nums[j]) >= valueDifference:
+                return [i,j]
+            else:
+                j+=1
+            if j>=len(nums):
+                i+=1
+                j=0
+
+
+
+        return [-1,-1]
