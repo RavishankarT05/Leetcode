bd29e725c189c3e9c39fcd0563af437a7bf2d19b
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-15 12:22:31 +0530

    Time: 11 ms (10.93%) | Memory: 12.4 MB (56.68%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.py
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.py
@@ -6,12 +6,3 @@ class Solution(object):
             nums.remove(min(nums))
             nums.remove(max(nums))
         return min(a)
-        
-        # a = []
-        # for i in range(len(nums) // 2):
-        #     mn = min(nums)
-        #     mx = max(nums)
-        #     a.append((mn + mx) / 2.0)
-        #     nums.remove(mn)
-        #     nums.remove(mx)
-        # return min(a) 
