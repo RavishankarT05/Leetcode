c674b570e3110174c8ba354ebf2f24dd39908366
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-16 22:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (57.40%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -3,12 +3,10 @@ class Solution(object):
         word=list(word)
         a=[]
         b=word
-        print(b)
         for i in b:
             if i==ch:
                 a.append(i)
                 word=word[1::]
-                print(a)
                 a=a[::-1]
                 return "".join(a)+"".join(word)
             else:
