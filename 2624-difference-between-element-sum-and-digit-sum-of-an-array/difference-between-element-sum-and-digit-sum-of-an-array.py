6a54009aae454723689f851167b7db26eae3e8dd
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-18 22:14:35 +0530

    Time: 71 ms (11.23%) | Memory: 12.5 MB (64.82%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.py
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -1,7 +1,8 @@
 class Solution(object):
     def differenceOfSum(self, nums):
-        a=sum(nums)
+        a=0
         b=0
         for i in nums:
+            a+=i
             b+=sum(map(int,str(i)))
         return abs(a-b)
