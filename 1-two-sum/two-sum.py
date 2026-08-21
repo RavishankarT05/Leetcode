e386e815400cd18f8a6f0ad6de37aedc9867defe
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-21 21:50:27 +0530

    Time: 111 ms (43.09%) | Memory: 12.7 MB (99.84%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,20 +1,22 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-        # num=sorted(nums)
-        # a,b=0,len(nums)-1
-        # while a<b:
-        #     if (num[a]+num[b])==target:
-        #         z=nums.index(num[a])
-        #         y=nums.index(num[b])
-        #         return [z,y]
-        #     elif (num[a]+num[b])<target:
-        #         a+=1
-        #     elif (num[a]+num[b])>target:
-        #         b-=1
-            
+    #     for i in range(len(nums)-1):
+    #         for j in range(i+1,len(nums)):
+    #             if nums[i]+nums[j]==target:
+    #                 return [i,j]
+        num=sorted(nums)
+        a,b=0,len(num)-1
+        while a<b:
+            if (num[a]+num[b])==target:
+                print(num)
+                print(nums)
+                c=nums.index(num[a])
+                nums[c]=-1
+                d=nums.index(num[b])
+                return [c,d]
+            elif (num[a]+num[b])<target:
+                a+=1
+            elif (num[a]+num[b])>target:
+                b-=1
 
         
