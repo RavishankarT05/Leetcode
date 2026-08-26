82a0c2f182d956b5bd076655998c5e25ec27c60a
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-26 21:50:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (90.06%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.py
+++ 3312-number-of-changing-keys/number-of-changing-keys.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def countKeyChanges(self, s):
-        a,b=0,1
         count=0
-        while b<len(s):
-            if s[a].lower()==s[b].lower():
-                pass
-            else:
+        for i in range(len(s)-1):
+            if s[i].lower()!=s[i+1].lower():
                 count+=1
-            a+=1
-            b+=1
         return count
 
 
