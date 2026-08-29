c495ccdd1a313f5bb099b4a5e326244c8a8c1800
RavishankarT05 <ravishankart05122007@gmail.com>
2026-08-29 14:45:17 +0530

    Time: 12 ms (50.50%) | Memory: 12.9 MB (12.29%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,15 +1,17 @@
 class Solution(object):
     def findMissingAndRepeatedValues(self, grid):
-        a = sum(grid, [])
-        a.sort()
-        z=[]
-        y=0
-        x=0
-        for i in range(len(a)) :
-            if a[i] not in z:
-                z.append(a[i])
-            else:
-                y+=a[i]
-            if i+1 not in a:
-                x+=i+1
-        return [y,x] 
+        return [sum(sum(grid, [])) - sum(set(sum(grid, []))),(set(range(1, len(sum(grid, [])) + 1)) - set(sum(grid, []))).pop()]
+        # a = sum(grid, [])
+        # a.sort()
+        # z=[]
+        # y=0
+        # x=0
+        # for i in range(len(a)) :
+        #     if a[i] not in z:
+        #         z.append(a[i])
+        #     else:
+        #         y+=a[i]
+        #     if i+1 not in a:
+        #         x+=i+1
+        # return [y,x] 
+        
