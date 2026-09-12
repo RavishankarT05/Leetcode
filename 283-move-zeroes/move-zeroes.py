69cff4d1f27f4886613a2ae8a6c1d23e259c79b9
RavishankarT05 <ravishankart05122007@gmail.com>
2026-09-12 19:07:50 +0530

    Time: 799 ms (5.00%) | Memory: 13.5 MB (77.01%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,3 +5,14 @@ class Solution(object):
                 nums.append(i)
                 nums.remove(i)
         return nums
+        # a,b=0,len(nums)-1
+        # while a<b:
+        #     if nums[a]!=0:
+        #         a+=1
+        #     if nums[b]==0:
+        #         b-=1
+        #     if nums[a]==0 and nums[b]!=0:
+        #         nums[a],nums[b]=nums[b],nums[a]
+        #         a+=1
+        #         b-=1
+        # return nums
