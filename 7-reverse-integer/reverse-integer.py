ec3cf7d31154d862fdabcf40fe107ce564c662ab
RavishankarT05 <ravishankart05122007@gmail.com>
2026-09-22 12:34:58 +0530

    Time: 11 ms (92.66%) | Memory: 12.4 MB (52.65%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -2,11 +2,11 @@ class Solution(object):
     def reverse(self, x):
         if x<0:
             x=-1*x
-            x=str(x)
+            x=list(map(int,str(x)))
             x=x[::-1]
             x=-1*int("".join(map(str, x)))
         else:
-            x=str(x)
+            x=list(map(int,str(x)))
             x=x[::-1]
             x=int("".join(map(str, x)))
         if x>2147483647 or x<-2147483648:
