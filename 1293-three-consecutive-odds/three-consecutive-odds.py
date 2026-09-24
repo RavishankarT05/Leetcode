880d004ac69b94b2f7c3fd525b9e13e3d5767b18
RavishankarT05 <ravishankart05122007@gmail.com>
2026-09-24 14:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (50.45%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        count=0
+        for i in arr:
+            if i%2!=0:
+                count+=1
+            else:
+                count=0
+            if count==3:
+                return True
+        return False
+            
+            
