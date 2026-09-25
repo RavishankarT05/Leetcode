71dc90e506e9bf3fee7915c589c45a70e97c714f
RavishankarT05 <ravishankart05122007@gmail.com>
2026-09-25 19:05:21 +0530

    Time: 19 ms (22.01%) | Memory: 14.1 MB (97.95%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def sortArrayByParityII(self, nums):
         a,b=0,1
-        while b<len(nums) and a<len(nums):
+        while a<len(nums) and b<len(nums):
             if nums[a]%2!=0:
                 if nums[b]%2==0:
                     nums[a],nums[b]=nums[b],nums[a]
